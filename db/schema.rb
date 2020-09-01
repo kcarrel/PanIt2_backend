c2cc94907dbf07dfcb1e1a1ad09b18179019556f
Katie Carrel <katiecarrel@comcast.net>
2020-09-01 14:40:50 -0700

    added collection resource and updated sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_212404) do
+ActiveRecord::Schema.define(version: 2020_09_01_212826) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "collections", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_collections_on_user_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
