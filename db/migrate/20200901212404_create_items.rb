49d515402e9154545ac0e37aba731e5cf904a500
Katie Carrel <katiecarrel@comcast.net>
2020-09-01 14:25:18 -0700

    add item resource

---
+++ db/migrate/20200901212404_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :brand
+      t.string :product_type
+      t.date :open_on
+      t.date :expiration
+      t.text :notes
+      t.integer :rating
+      t.boolean :favorite
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_194646) do
+ActiveRecord::Schema.define(version: 2020_09_01_212404) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "brand"
+    t.string "product_type"
+    t.date "open_on"
+    t.date "expiration"
+    t.text "notes"
+    t.integer "rating"
+    t.boolean "favorite"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
