432f8f84fbba51fcbf5ba3be212011d89452e2b2
Katie Carrel <katiecarrel@comcast.net>
2020-09-03 10:36:52 -0700

    add routing for user login, missed belongs to for user and controller functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    has_one :collection
+    has_many :items, through: :collection
+    has_many :favorites
+    
     has_secure_password
     validates :email, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :collections
   resources :items
-  resources :users
+  resources :users, only: [:create]
+      post '/login', to: 'auth#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
